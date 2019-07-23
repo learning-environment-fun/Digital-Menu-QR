26f3029b2b4c38cbd57a960829e660be36b288cb
Freddyvl <freddy.vl@me.com>
2019-07-23 12:25:30 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'restaurants#show'
 
+  #
+  get "/tables/:id/orders/new", to ':orders#new'
+  post "/tables/:id/orders", to 'orders#create'
 
+resources :restaurants, only: [:show] do
+  resources :items, only: [:show]
+  resources :orders, only: [:show, :update, :destroy]
+ end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
