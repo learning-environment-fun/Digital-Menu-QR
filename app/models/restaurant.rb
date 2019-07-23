d1e6edcf17994e279473738bc30059ff531c3902
Felix Naerger <fnaerger@gmail.com>
2019-07-23 18:07:00 +0200

    fixed seeds, set up associations and validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
 class Restaurant < ApplicationRecord
+  has_many :tables
+
+  # Presence
+  validates :name, presence: true
+  validates :address, presence: true
+
+  # Unique
+  validates :name, uniqueness: true
+  validates :address, uniqueness: true
 end
