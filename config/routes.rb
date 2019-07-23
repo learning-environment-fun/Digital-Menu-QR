d18988107991763d37111d290627530979784087
Freddyvl <freddy.vl@me.com>
2019-07-23 13:20:11 +0200

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
+  get 'items/orders'
+  get 'items/restaurants'
   devise_for :users
   root to: 'restaurants#show'
 
-  #
-  get "/tables/:id/orders/new", to ':orders#new'
-  post "/tables/:id/orders", to 'orders#create'
+  #1 after scanning a new order will be created, than redicrection to 'show restaurants items'
+  get "/tables/:id/orders/new", to: 'orders#new'
+  post "/tables/:id/orders", to: 'orders#create'
 
+#display the items of the restaurant
+# restaurants/1
 resources :restaurants, only: [:show] do
+
+  #display a specific item
+  # restaurants/1/items/:id
   resources :items, only: [:show]
+  #possibility to update/update/destroy
+  # restaurants/1/items/:id/show
   resources :orders, only: [:show, :update, :destroy]
  end
 
