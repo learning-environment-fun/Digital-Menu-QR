d1fa1c9041e626725b10b22e22551ad5c74f62e6
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-24 15:24:08 +0200

    faker rendering

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.3'
 # gem 'dotenv-rails', groups: [:development, :test] --> already listed at bottom of file
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
+gem 'faker', require: true
 
 gem 'bootsnap', require: false
 gem 'devise'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,51 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
-
-  def home
+  #skip_before_action :authenticate_user!, only: [:home]
+  class << self
+    attr_reader :dummy_items, :cart_props
   end
-
-  def cart
-
-  end
-
-  def pay
-
-  end
-
-  def feedback
-
-  end
-
-  private 
 
   class DummyItem
-    def dollars 
-      @price.to_f / 100
-    end
     
+    attr_reader :title, :price
+    
+    def dollars 
+      (@price.to_f / 100.to_f).round(2)
+    end
+
     def initialize(title = "title", price = 230)
       @title = title
       @price = price
     end
   end
+
+  def self.seed_items(n)
+    items = []
+    
+    n.times do 
+      items << DummyItem.new(Faker::Games::Pokemon.name, 100 + Random.rand(2000))
+    end
+    return items
+  end
+
+  @dummy_items = PagesController.seed_items(3 + Random.rand(7))
+
+  @cart_props = { items: @dummy_items, total: @dummy_items.map { |i| i.price }.reduce(:+).to_f / 100.to_f }
+
+  def home
+  end
+
+  def cart
+    #@items = self.class.seed_items(3 + Random.rand(7))
+    #@items = PagesController.seed_items(3 + Random.rand(7))
+    @cart_props = self.class.cart_props
+  end
+
+  def pay
+    @items = PagesController.seed_items(3 + Random.rand(7))
+  end
+
+  def feedback
+    @items = PagesController.seed_items(3 + Random.rand(7))
+  end
+   
 end
