65852a73c868e06888283d653baaa2db140fbc17
Freddyvl <freddy.vl@me.com>
2019-07-31 10:01:37 +0200

    added new fixed cart sumemry

--- app/controllers/order_Items_controller.rb
+++ app/controllers/order_Items_controller.rb
@@ -3,34 +3,30 @@ class OrderItemsController < ApplicationController
 
 
   def create
-
   @order_item = OrderItem.new(order_items_params)
   @order = Order.find(params[:order_id])
   if @order_item.save
-    if params[:add_to_cart]
-      redirect_to cart_order_path(@order.id, @orderItem)
+      redirect_to cart_order_path(@order, @orderItem)
 
-    else params[:order_now]
-      redirect_to pay_path(@order.id, @orderItem)
-
-    end
   else
     render "items/show"
   end
   end
 
-def destroy
- @order_item = OrderItem.find(params[:id])
- @order_item.destroy
- redirect_to cart_order_path(@order_item.order, @order_item)
-end
+  def destroy
+    order_item = OrderItem.find(params[:id])
+    order = Order.find(params[:order_id])
+
+    order_item.destroy
+
+    redirect_to cart_order_path(order)
+  end
 
   private
 
 def order_items_params
-    permitted_params = params.require(:order_item).permit(:quantity, :order_id, :item_id, :item_price)
+    params.require(:order_item).permit(:quantity, :order_id, :item_id, :item_price)
   end
-
 
 
 end
