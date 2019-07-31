1a66e7f886c99d2d2a8f52117dad51e548db86de
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-31 15:12:18 +0200

    closed method

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,27 +5,28 @@ class PaymentsController < ApplicationController
   end
 
   def create
-     customer = Stripe::Customer.create(
+    customer = Stripe::Customer.create(
       source: params[:stripeToken],
       email:  params[:stripeEmail]
     )
 
-  charge = Stripe::Charge.create(
-    customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @order.total_cost,
-    description:  "Payment for for order #{@order.id}",
-    currency:     "eur"
-  )
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @order.total_cost,
+      description:  "Payment for for order #{@order.id}",
+      currency:     "eur"
+    )
 
-  @order.update(payment: charge.to_json, state: 'paid')
-  redirect_to order_path(@order)
+    @order.update(payment: charge.to_json, state: 'paid')
+    redirect_to order_path(@order)
 
-  rescue Stripe::CardError => e
-    flash[:alert] = e.message
-    redirect_to new_order_payment_path(@order)
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_order_payment_path(@order)
+    end
+
+    redirect_to feedback_path(@order)
   end
-
-  redirect_to "/feedback/#{@order.id}"
 
 private
 
