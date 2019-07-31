f9566095a2334e3f0ff09a2f3d2aa9f83df65274
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-31 16:27:10 +0200

    fixed indents/missing end in order_items_controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,16 +1,20 @@
 class OrderItemsController < ApplicationController
-
-
-
   def create
-  @order_item = OrderItem.new(order_items_params)
-  @order = Order.find(params[:order_id])
-  if @order_item.save
-      redirect_to cart_order_path(@order, @orderItem)
-
-  else
-    render "items/show"
-  end
+    @order_item = OrderItem.new(order_items_params)
+    @order = Order.find(params[:order_id])
+    if @order.save
+      if params[:add_to_cart]
+        redirect_to order_path(@order)
+      else params[:order_now]
+        redirect_to pay_path(@order.id, @orderItem)
+      end
+      
+      if @order_item.save
+        redirect_to cart_order_path(@order, @orderItem)
+      else
+        render "items/show"
+      end
+    end
   end
 
   def destroy
@@ -24,9 +28,7 @@ class OrderItemsController < ApplicationController
 
   private
 
-def order_items_params
+  def order_items_params
     params.require(:order_item).permit(:quantity, :order_id, :item_id, :item_price)
   end
-
-
 end
