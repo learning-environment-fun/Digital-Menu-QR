eafb28a85c6c7c5f2aaf748eb14df07ed3383ea9
Charles Shorter <cshorter.mc@gmail.com>
2019-07-31 11:17:49 +0200

    fixed seed again

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :orders, :dependent => :destroy
-  has_many :restaurants, :dependent => :destroy
+  has_many :restaurants
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
