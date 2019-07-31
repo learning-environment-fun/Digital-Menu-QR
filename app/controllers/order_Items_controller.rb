9c55451336c36ce24b3c419958506421ab2302b0
Felix Naerger <fnaerger@gmail.com>
2019-07-31 10:43:13 +0200

    making the kitchen view sexy

--- app/controllers/order_Items_controller.rb
+++ app/controllers/order_Items_controller.rb
@@ -6,9 +6,9 @@ class OrderItemsController < ApplicationController
 
   @order_item = OrderItem.new(order_items_params)
   @order = Order.find(params[:order_id])
-  if @order_item.save
+  if @order.save
     if params[:add_to_cart]
-      redirect_to cart_order_path(@order.id, @orderItem)
+      redirect_to order_path(@order)
 
     else params[:order_now]
       redirect_to pay_path(@order.id, @orderItem)
@@ -19,18 +19,11 @@ class OrderItemsController < ApplicationController
   end
   end
 
-def destroy
- @order_item = OrderItem.find(params[:id])
- @order_item.destroy
- redirect_to cart_order_path(@order_item.order, @order_item)
-end
-
   private
 
 def order_items_params
     permitted_params = params.require(:order_item).permit(:quantity, :order_id, :item_id, :item_price)
   end
-
 
 
 end
